8fdd722c7be996e19fc9192296ce1adaeb4641fd
Hopeless2 <89778966354@mail.ru>
2021-11-01 22:48:15 +0300

    Commit init

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps ops = new BinOps();
+        String arg0 = "10110";
+        String arg1 = "1";
+        System.out.println(ops.mult(arg0, arg1));
+        System.out.println(ops.sum(arg0, arg1));
+
+    }
+}
